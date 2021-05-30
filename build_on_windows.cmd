86f89555bdd192c0ae38198c7206198e46587cb7
Rajat Rajput <rajputrajat@gmail.com>
2021-05-30 20:29:05 +0530

    running :-)

--- build_on_windows.cmd
+++ build_on_windows.cmd
@@ -1,2 +1,3 @@
 cargo build
+cp target\debug\hello_c_world_dynamic.dll .
 clang++.exe .\hello.cpp .\target\debug\hello_c_world_dynamic.dll.lib -I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.18362.0\ucrt"
